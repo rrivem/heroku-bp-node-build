b66e06a2cf1af30b06e1c9c83b3ae75481355762
carloluis <carloluisr@gmail.com>
2017-07-29 23:21:01 -0300

    code consistency - environment script

--- lib/environment.sh
+++ lib/environment.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-export_env_dir() {
+function export_env_dir() {
 	local env_dir="$1"
 	local whitelist_regex="${2:-''}"
 	local blacklist_regex="${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH|LANG)$'}"
+
 	if [[ -d "$env_dir" ]]; then
 		for e in $(ls "$env_dir"); do
 			echo "$e=$(cat $env_dir/$e)"
